5804a697d7391c2e94f03f2bb6bc5ad98aff0730
Felipe Vogel <fps.vogel@gmail.com>
2024-02-13 21:12:49 -0500

    Add Ruby versions of variations 4-6

--- tennis/start.rb
+++ tennis/start.rb
@@ -1,5 +1,3 @@
-# TODO: add versions 4, 5, && 6 from https://github.com/emilybache/Tennis-Refactoring-Kata/tree/main/typescript/src
-
 class TennisGame
   def won_point
     raise NotImplementedError, "#{self.class} should have implemented #{__method__}"
@@ -163,6 +161,8 @@ class TennisGame2 < TennisGame
     result
   end
 
+  private
+
   def set_player1_score(number)
     (0..number).each do |i|
       player1_score
@@ -215,3 +215,295 @@ class TennisGame3 < TennisGame
     end
   end
 end
+
+class TennisGame4 < TennisGame
+  attr_reader :server, :receiver, :server_score, :receiver_score
+
+  def initialize(player1, player2)
+    @server = player1
+    @receiver = player2
+    @server_score = 0
+    @receiver_score = 0
+  end
+
+  def score
+    result = Deuce.new(
+      self,
+      GameServer.new(
+        self,
+        GameReceiver.new(
+          self,
+          AdvantageServer.new(
+            self,
+            AdvantageReceiver.new(
+              self,
+              DefaultResult.new(self),
+            ),
+          ),
+        ),
+      ),
+    ).result
+
+    result.format
+  end
+
+  def receiver_has_advantage
+    @receiver_score >= 4 && (@receiver_score - @server_score) == 1
+  end
+
+  def server_has_advantage
+    @server_score >= 4 && (@server_score - @receiver_score) == 1
+  end
+
+  def receiver_has_won
+    @receiver_score >= 4 && (@receiver_score - @server_score) >= 2
+  end
+
+  def server_has_won
+    @server_score >= 4 && (@server_score - @receiver_score) >= 2
+  end
+
+  def deuce?
+    @server_score >= 3 && @receiver_score >= 3 && (@server_score == @receiver_score)
+  end
+
+  def won_point(player_name)
+    if player_name == "player1"
+      @server_score += 1
+    else
+      @receiver_score += 1
+    end
+  end
+end
+
+class TennisResult
+  attr_reader :server_score, :receiver_score
+
+  def initialize(server_score, receiver_score)
+    @server_score = server_score
+    @receiver_score = receiver_score
+  end
+
+  def format
+    return @server_score if @receiver_score == ""
+
+    if @server_score == @receiver_score
+      "#{@server_score}-All"
+    else
+      "#{@server_score}-#{@receiver_score}"
+    end
+  end
+end
+
+class Deuce
+  def initialize(game, next_result)
+    @game = game
+    @next_result = next_result
+  end
+
+  def result
+    if @game.deuce?
+      TennisResult.new("Deuce", "")
+    else
+      @next_result.result
+    end
+  end
+end
+
+class GameServer
+  def initialize(game, next_result)
+    @game = game
+    @next_result = next_result
+  end
+
+  def result
+    if @game.server_has_won
+      TennisResult.new("Win for #{@game.server}", "")
+    else
+      @next_result.result
+    end
+  end
+end
+
+class GameReceiver
+  def initialize(game, next_result)
+    @game = game
+    @next_result = next_result
+  end
+
+  def result
+    if @game.receiver_has_won
+      TennisResult.new("Win for #{@game.receiver}", "")
+    else
+      @next_result.result
+    end
+  end
+end
+
+class AdvantageServer
+  def initialize(game, next_result)
+    @game = game
+    @next_result = next_result
+  end
+
+  def result
+    if @game.server_has_advantage
+      TennisResult.new("Advantage #{@game.server}", "")
+    else
+      @next_result.result
+    end
+  end
+end
+
+class AdvantageReceiver
+  def initialize(game, next_result)
+    @game = game
+    @next_result = next_result
+  end
+
+  def result
+    if @game.receiver_has_advantage
+      TennisResult.new("Advantage #{@game.receiver}", "")
+    else
+      @next_result.result
+    end
+  end
+end
+
+class DefaultResult
+  def initialize(game)
+    @scores = ["Love", "Fifteen", "Thirty", "Forty"]
+    @game = game
+  end
+
+  def result
+    TennisResult.new(@scores[@game.server_score], @scores[@game.receiver_score])
+  end
+end
+
+class TennisGame5 < TennisGame
+  attr_reader :player1_name, :player2_name, :player1_score, :player2_score
+
+  def initialize(player1_name, player2_name)
+    @player1_name = player1_name
+    @player2_name = player2_name
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+  def score
+    p1 = @player1_score
+    p2 = @player2_score
+
+    while p1 > 4 || p2 > 4
+      p1 -= 1
+      p2 -= 1
+    end
+
+    lookup = {
+      0 => {
+        0 => "Love-All",
+        1 => "Love-Fifteen",
+        2 => "Love-Thirty",
+        3 => "Love-Forty",
+        4 => "Win for #{@player2_name}",
+      },
+      1 => {
+        0 => "Fifteen-Love",
+        1 => "Fifteen-All",
+        2 => "Fifteen-Thirty",
+        3 => "Fifteen-Forty",
+        4 => "Win for #{@player2_name}",
+      },
+      2 => {
+        0 => "Thirty-Love",
+        1 => "Thirty-Fifteen",
+        2 => "Thirty-All",
+        3 => "Thirty-Forty",
+        4 => "Win for #{@player2_name}",
+      },
+      3 => {
+        0 => "Forty-Love",
+        1 => "Forty-Fifteen",
+        2 => "Forty-Thirty",
+        3 => "Deuce",
+        4 => "Advantage #{@player2_name}",
+      },
+      4 => {
+        0 => "Win for #{@player1_name}",
+        1 => "Win for #{@player1_name}",
+        2 => "Win for #{@player1_name}",
+        3 => "Advantage #{@player1_name}",
+        4 => "Deuce",
+      }
+    }
+
+    lookup[p1][p2]
+  end
+
+  def won_point(player_name)
+    if player_name == "player1"
+      @player1_score += 1
+    else
+      @player2_score += 1
+    end
+  end
+end
+
+class TennisGame6 < TennisGame
+  attr_reader :player1_name, :player2_name, :player1_score, :player2_score
+
+  def initialize(player1_name, player2_name)
+    @player1_name = player1_name
+    @player2_name = player2_name
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+  def won_point(player_name)
+    player_name == "player1" ? @player1_score += 1 : @player2_score += 1
+  end
+
+  def score
+    if @player1_score == @player2_score
+      tie_score
+    elsif @player1_score >= 4 || @player2_score >= 4
+      end_game_score
+    else
+      regular_score
+    end
+  end
+
+  private
+
+  def tie_score
+    case @player1_score
+    when 0
+      "Love-All"
+    when 1
+      "Fifteen-All"
+    when 2
+      "Thirty-All"
+    else
+      "Deuce"
+    end
+  end
+
+  def end_game_score
+    if @player1_score - @player2_score == 1
+      "Advantage #{@player1_name}"
+    elsif @player1_score - @player2_score == -1
+      "Advantage #{@player2_name}"
+    elsif @player1_score - @player2_score >= 2
+      "Win for #{@player1_name}"
+    else
+      "Win for #{@player2_name}"
+    end
+  end
+
+  def regular_score
+    score_names = %w[Love Fifteen Thirty Forty]
+    "#{score_names[@player1_score]}-#{score_names[@player2_score]}"
+  end
+end
+
